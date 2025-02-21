cbb5cec354292d16b0ac0b6ed1a08a5df9f5890f
Mahedihasanimam <mdmehedihasen678@gmail.com>
2025-02-21 12:36:40 +0600

    login page added

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,24 @@
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import tw from '../lib/tailwind';
 
-const Header = () => {
+interface HeaderProps {
+  title: string;
+  subtitle?: string;
+}
+const Header = ({title, subtitle}: HeaderProps) => {
   return (
     <LinearGradient
       locations={[5, 0.5]}
-      colors={['#0D3050', '#19578F']} 
-      start={{ x: 0.2, y: 0.5 }}
-      end={{ x: 0.2, y: 0.5 }} 
-      style={tw.style('h-[200px]  w-full')} 
-    >
-      <Text style={tw`text-white text-lg`}>Hello, Gradient!</Text>
+      colors={['#0D3050', '#19578F']}
+      start={{x: 0.2, y: 0.5}}
+      end={{x: 0.2, y: 0.5}}>
+      <View style={tw`px-4 pb-[40px] pt-[80px] `}>
+        <Text style={tw`text-[#E7E7E9] text-[40px] font-bold`}>{title}</Text>
+        <Text style={tw`text-xs text-[#B6B6BA] font-normal pt-1`}>
+          {subtitle}
+        </Text>
+      </View>
     </LinearGradient>
   );
 };
--- src/screens/AuthScreen/Login.tsx
+++ src/screens/AuthScreen/Login.tsx
@@ -1,15 +1,168 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import tw from '../../lib/tailwind'
-import Header from '../../components/Header'
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Pressable,
+} from 'react-native';
+import React, { useState } from 'react';
+import { useForm, Controller } from 'react-hook-form';
+import tw from '../../lib/tailwind';
+import Header from '../../components/Header';
+import { SvgXml } from 'react-native-svg';
+import { EyeIcon, EyeOffIcon, LockIcon, LoginIcon, rememberme } from '../../assets/Icons';
+import { useNavigation } from '@react-navigation/native';
 
-const Login = () => {
-  return (
-    <View>
-        <Header/>
-      <Text style={tw`text-4xl `}>Login</Text>
-    </View>
-  )
+
+interface LoginProps {
+  email: string;
+  password: string;
 }
 
-export default Login
+const Login = () => {
+
+  const Navigation = useNavigation();
+  const {
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginProps>();
+
+  const [showPassword, setShowPassword] = useState(false);
+  const [rememberMe, setRememberMe] = useState(false);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword((prev) => !prev);
+  };
+
+  const onSubmit = (data: LoginProps) => {
+    console.log('Form Data:', data, 'Remember Me:', rememberMe);
+  };
+
+  return (
+    <View style={tw`flex-1 `}>
+      <Header
+        title="Sign-in to your account..."
+       
+      />
+
+      <View style={tw`px-4 pt-[80% ] pb-6`}>
+        {/* Email Input */}
+        <View style={tw`mb-4`}>
+          <Text style={tw`text-[#41414D] text-[14px] font-normal pb-[4px]`}>
+            Email
+          </Text>
+          <Controller
+            control={control}
+            name="email"
+            rules={{
+              required: 'Email is required',
+              pattern: {
+                value: /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+                message: 'Invalid email address',
+              },
+            }}
+            render={({ field: { onChange, onBlur, value } }) => (
+              <TextInput
+                style={tw`border p-2 h-[48px] text-[#41414D] rounded-md focus:border-2 border-[#4B8FCB] ${
+                  errors.email ? 'border-red-500' : 'border-gray-300'
+                }`}
+                placeholder="Enter your email"
+                onBlur={onBlur}
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+          {errors.email && (
+            <Text style={tw`text-red text-xs`}>{errors.email.message}</Text>
+          )}
+        </View>
+
+        {/* Password Input */}
+        <View style={tw`mb-4`}>
+          <Text style={tw`text-[#41414D] text-[14px] font-normal pb-[4px]`}>
+            Password
+          </Text>
+          <View style={tw`relative flex-row items-center border px-2 rounded-md ${errors.password ? 'border-red-500' : 'border-gray-300'}`}>
+            {/* Lock Icon */}
+            <SvgXml xml={LockIcon} width={20} height={20} style={tw`mr-2`} />
+
+            <Controller
+              control={control}
+              name="password"
+              rules={{ required: 'Password is required' }}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextInput
+                  style={tw`flex-1 text-[#41414D] h-[48px]`}
+                  placeholder="Enter your password"
+                  secureTextEntry={!showPassword}
+                  onBlur={onBlur}
+                  onChangeText={onChange}
+                  value={value}
+                />
+              )}
+            />
+
+            {/* Eye Toggle Icon */}
+            <Pressable onPress={togglePasswordVisibility}>
+              <SvgXml
+                xml={showPassword ? EyeOffIcon : EyeIcon}
+                width={20}
+                height={20}
+                style={tw`ml-2`}
+              />
+            </Pressable>
+          </View>
+
+          {errors.password?.message && (
+            <Text style={tw`text-red text-xs`}>{errors.password.message}</Text>
+          )}
+        </View>
+
+        {/* Remember Me & Forgot Password */}
+        <View style={tw`flex-row justify-between items-center mb-6`}>
+          <TouchableOpacity
+            onPress={() => setRememberMe(!rememberMe)}
+            style={tw`flex-row items-center`}>
+            {
+              rememberMe ? <SvgXml xml={rememberme} /> : <View
+              style={tw`w-[20px] h-[20px] border rounded mr-2 border-gray-400`}
+            />
+            }
+            <Text style={tw`text-[#41414D] text-sm pl-1`}>Remember Me</Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity>
+            <Text style={tw`text-[#4B8FCB] text-sm font-semibold`}>
+              Forgot Password?
+            </Text>
+          </TouchableOpacity>
+        </View>
+
+        {/* Submit Button */}
+        <TouchableOpacity
+          style={tw`bg-primary p-3 rounded-md flex flex-row items-center justify-center`}
+          onPress={handleSubmit(onSubmit)}>
+          <View style={tw`flex flex-row items-center justify-center`}>
+            <Text style={tw`text-[#E7E7E9] text-[16px] font-bold pr-1`}>
+              Sign in
+            </Text>
+            <SvgXml xml={LoginIcon} />
+          </View>
+        </TouchableOpacity>
+
+
+        <View style={tw`flex-row items-center justify-center mt-[10%]`}>
+          <Text style={tw`text-[#41414D] text-sm font-semibold`}>Don't have an account?</Text>
+          <TouchableOpacity onPress={() => Navigation.navigate('Register')}>
+            <Text style={tw`text-[#4B8FCB] text-sm font-semibold pl-1`}>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+
+      </View>
+    </View>
+  );
+};
+
+export default Login;
